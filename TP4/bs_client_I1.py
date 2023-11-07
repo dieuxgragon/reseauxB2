fe3c92765e5ab411bc61a0ffec358cf102acfc7d
dieuxgragon <enzonussbaum33@gmail.com>
2023-11-07 16:22:50 +0100

    TP4

---
+++ TP4/bs_client_I1.py
@@ -0,0 +1,30 @@
+import socket
+
+host ='10.1.1.10'
+port = 13337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((host, port))
+
+s.listen(1)
+
+conn, addr = s.accept()
+print('Connected by', addr)
+
+while True:
+
+    try:
+        data = conn.recv(1024)
+        if not data: break
+        print(f"Données reçues du client : {data}")
+
+        message = "Meooooo "
+        conn.sendall(message.encode())
+     
+    except socket.error:
+        print("Error Occured.")
+        break
+
+    conn.close()
+
