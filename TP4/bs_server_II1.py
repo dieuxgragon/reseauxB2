6bb98c31589f6f8a8e1a92e6e023a1d9a8ce1d24
dieuxgragon <enzonussbaum33@gmail.com>
2023-11-27 10:12:26 +0100

    Args

--- TP4/bs_server_II1.py
+++ TP4/bs_server_II1.py
@@ -1,19 +1,16 @@
 import argparse
-import sys
+import socket
 
 def check_port(value):
     try:
         port = int(value)
         if port < 0 or port > 65535:
-            print("ERROR: Le port spécifié n'est pas un port possible (de 0 à 65535).")
-            sys.exit(1)
+            raise argparse.ArgumentTypeError("Le port spécifié n'est pas un port possible (de 0 à 65535).")
         elif port <= 1024:
-            print("ERROR: Le port spécifié est un port privilégié. Spécifiez un port au-dessus de 1024.")
-            sys.exit(2)
+            raise argparse.ArgumentTypeError("Le port spécifié est un port privilégié. Spécifiez un port au-dessus de 1024.")
         return port
     except ValueError:
-        print("ERROR: Le port doit être un entier.")
-        sys.exit(1)
+        raise argparse.ArgumentTypeError("Le port doit être un entier.")
 
 parser = argparse.ArgumentParser(description="Description de votre commande")
 
@@ -23,6 +20,16 @@ parser.add_argument(
     default=13337,
     help="Spécifier un numéro de port (par défaut : 13337)"
 )
+
 args = parser.parse_args()
 
-print(f"Port spécifié : {args.port}")
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('10.1.2.10', args.port))
+server_socket.listen(1)
+
+print(f"Serveur en attente de connexions sur le port {args.port} depuis l'adresse IP 10.1.2.10")
+
+while True:
+    client_socket, client_address = server_socket.accept()
+    print(f"Connexion établie avec {client_address}")
+    client_socket.close()
