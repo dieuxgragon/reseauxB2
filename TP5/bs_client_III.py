c47338c736ec3f5776a49e2cd751b5293858cf2a
dieuxgragon <enzonussbaum33@gmail.com>
2023-11-30 15:20:40 +0100

    Args

--- TP5/bs_client_III.py
+++ TP5/bs_client_III.py
@@ -1,15 +1,4 @@
 import socket
-
-def check_nbr():
-    try:
-        msg = input()
-        # Vérification si le nombre est inférieur ou égal à 4294967295
-        if int(msg) > 4294967295:
-            print("Nombre trop grand. Veuillez entrer un nombre inférieur ou égal à 4294967295.")
-        else:
-            return input
-    except ValueError:
-        print("Veuillez entrer un nombre valide.")
     
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('10.1.1.10', 13337))
@@ -18,6 +7,17 @@ s.send('Hello'.encode())
 # On reçoit la string Hello
 data = s.recv(1024)
 
+while True:
+    try:
+        msg = input("Calcul à envoyer (max 4294967295): ")
+        # Vérification si le nombre est inférieur ou égal à 4294967295
+        if int(msg) > 4294967295:
+            print("Nombre trop grand. Veuillez entrer un nombre inférieur ou égal à 4294967295.")
+        else:
+            break
+    except ValueError:
+        print("Veuillez entrer un nombre valide.")
+        
 # Récupération d'une string utilisateur
 msg = input("Calcul à envoyer: ")
 
