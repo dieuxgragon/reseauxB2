0f5defa13d25e2d3d1ce121b90e1ee74fee022cc
dieuxgragon <enzonussbaum33@gmail.com>
2023-11-30 15:34:35 +0100

    Args

--- TP5/bs_client_III.py
+++ TP5/bs_client_III.py
@@ -1,7 +1,7 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 9999))
+s.connect(('10.1.1.10', 13337))
 s.send('Hello'.encode())
 
 data = s.recv(1024)
