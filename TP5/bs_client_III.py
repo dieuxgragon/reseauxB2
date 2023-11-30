2294001230e250372d050d2e1f91435522895364
dieuxgragon <enzonussbaum33@gmail.com>
2023-11-30 15:33:56 +0100

    Args

--- TP5/bs_client_III.py
+++ TP5/bs_client_III.py
@@ -1,32 +1,40 @@
 import socket
-import sys
-    
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('10.1.1.10', 13337))
+s.connect(('127.0.0.1', 9999))
 s.send('Hello'.encode())
 
-# On reçoit la string Hello
 data = s.recv(1024)
 
 while True:
-    msg = input("Calcul :")
-    try:
-        # Vérification si le nombre est inférieur ou égal à 4294967295
-        num = int(msg)
-        if 0 <= num <= 4294967295:
+    user_input = input("Calcul à envoyer: ")
+
+    if len(user_input) <= 11:  
+
+        parts = user_input.split()
+        if len(parts) == 3 and parts[0].isdigit() and parts[2].isdigit() and parts[1] in ['+', '-', '*']:
             break
         else:
-            print("Nombre hors de la plage autorisée. Veuillez entrer un nombre entre 0 et 4294967295.")
-    except ValueError:
-        print("Veuillez entrer un nombre valide.")
-        
-# Récupération d'une string utilisateur
-msg = input("Calcul à envoyer: ")
+            print("Expression invalide. Veuillez entrer une expression arithmétique simple comme 'x opération y' (ex: 3 + 3).")
+    else:
+        print("Expression trop longue. Veuillez entrer une expression plus courte.")
 
-# On envoie
-s.send(msg.encode())
-
-# Réception et affichage du résultat
 s_data = s.recv(1024)
 print(s_data.decode())
 s.close()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('127.0.0.1', 9999))
+
+msg = input('Enter a message: ')
+
+encoded_msg = msg.encode('utf-8')
+
+msg_len = len(encoded_msg)
+
+header = msg_len.to_bytes(4, byteorder='big')
+
+payload = header + encoded_msg
+
+sock.send(payload)
+sock.close()
