328649da5223c828919e2abc52d8bf6e4e5f24d3
dieuxgragon <enzonussbaum33@gmail.com>
2023-11-30 14:52:02 +0100

    Args

--- TP5/bs_client_III.py
+++ TP5/bs_client_III.py
@@ -1,31 +1,20 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('10.1.1.10', 9999))
+s.connect(('127.0.0.1', 9999))
 s.send('Hello'.encode())
 
+# On reçoit la string Hello
 data = s.recv(1024)
 
+# Récupération d'une string utilisateur
 msg = input("Calcul à envoyer: ")
 
+# On envoie
 s.send(msg.encode())
 
+# Réception et affichage du résultat
 s_data = s.recv(1024)
 print(s_data.decode())
 s.close()
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('10.1.1.10', 9999))
-
-msg = input('Enter a message: ')
-
-encoded_msg = msg.encode('utf-8')
-
-msg_len = len(encoded_msg)
-
-header = msg_len.to_bytes(4, byteorder='big')
-
-payload = header + encoded_msg
-
-sock.send(payload)
-sock.close()
+    
