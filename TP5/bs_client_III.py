0c94cdda89f4e24a578ca804f9418a18bb84fe80
dieuxgragon <enzonussbaum33@gmail.com>
2023-11-30 15:37:09 +0100

    Args

--- TP5/bs_client_III.py
+++ TP5/bs_client_III.py
@@ -1,16 +1,19 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('10.1.1.10', 13337))
+s.connect(('127.0.0.1', 9999))
 s.send('Hello'.encode())
 
+# On reçoit la string Hello
 data = s.recv(1024)
 
+# Récupération d'une string utilisateur
+msg = input("Calcul à envoyer: ")
 while True:
     user_input = input("Calcul à envoyer: ")
-
-    if len(user_input) <= 11:  
-
+    # Vérification de la longueur de l'expression
+    if len(user_input) <= 11:  # Longueur maximale de 11 caractères pour "4294967295"
+        # Vérification si l'expression est valide
         parts = user_input.split()
         if len(parts) == 3 and parts[0].isdigit() and parts[2].isdigit() and parts[1] in ['+', '-', '*']:
             break
@@ -19,22 +22,10 @@ while True:
     else:
         print("Expression trop longue. Veuillez entrer une expression plus courte.")
 
+# On envoie
+s.send(msg.encode())
+
+# Réception et affichage du résultat
 s_data = s.recv(1024)
 print(s_data.decode())
 s.close()
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('127.0.0.1', 9999))
-
-msg = input('Enter a message: ')
-
-encoded_msg = msg.encode('utf-8')
-
-msg_len = len(encoded_msg)
-
-header = msg_len.to_bytes(4, byteorder='big')
-
-payload = header + encoded_msg
-
-sock.send(payload)
-sock.close()
