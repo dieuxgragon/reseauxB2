859121152ad548850c3b64164acba157f80ba2dc
dieuxgragon <enzonussbaum33@gmail.com>
2023-11-30 16:43:16 +0100

    Args

--- TP5/bs_client_III.py
+++ TP5/bs_client_III.py
@@ -5,8 +5,6 @@ s.connect(('10.1.1.10', 13337))
 s.send('Hello'.encode())
 
 # On reçoit la string Hello
-data = s.recv(1024)
-
 # Récupération d'une string utilisateur
 msg = input("Calcul à envoyer: ")
 
