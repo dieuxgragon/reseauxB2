892bb78dc7f1e1c481a1672ec6b20c5527768a40
dieuxgragon <enzonussbaum33@gmail.com>
2023-12-01 16:08:47 +0100

    Args

--- TP5/bs_client_III.py
+++ TP5/bs_client_III.py
@@ -6,10 +6,17 @@ s.connect(('10.1.1.10', 13337))
 # Récupération d'une string utilisateur
 msg = input("Calcul à envoyer: ")
 
-msg_len = len(msg)
-if msg_len > 3:
-    print("calcule trop long essaye a + b")
-    SystemExit
+while True:
+
+    try:
+        msg_len = len(msg)
+
+        if msg_len > 3:
+           print("calcule trop long essaye a + b")
+        break
+    except socket.error:
+        print("Error Occured.")
+        break
 
 payload = str(msg_len) + msg 
 
