0c12e917b6bec45565c83fe557a4828c14346811
dieuxgragon <enzonussbaum33@gmail.com>
2023-12-01 14:35:36 +0100

    Args

--- TP5/bs_client_III.py
+++ TP5/bs_client_III.py
@@ -6,9 +6,10 @@ s.connect(('10.1.1.10', 13337))
 # Récupération d'une string utilisateur
 msg = input("Calcul à envoyer: ")
 
-msg_len = len(msg)
+msg_encode = msg.encode('utf-8')
+msg_len = len(msg_encode)
 
-payload = str(msg_len) + msg
+payload = str(msg_len) + msg_encode
 
 s.send(payload.encode())
 # Réception et affichage du résultat
