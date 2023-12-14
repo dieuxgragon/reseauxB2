98be3321fca7f73dfd50b88e546474b97b5d654d
dieuxgragon <enzonussbaum33@gmail.com>
2023-12-14 14:59:41 +0100

    add info

--- TP7/ws_i_1_client.py
+++ TP7/ws_i_1_client.py
@@ -4,13 +4,14 @@ import websockets
 async def hello():
     uri = "ws://10.1.2.10:13337"
     async with websockets.connect(uri) as websocket:
-        name = input("What's your name? ")
+        while True:
+            name = input("What's your name ? ")
 
-        await websocket.send(name)
-        print(f">>> {name}")
+            await websocket.send(name)
+            print(f">>> {name}")
 
-        greeting = await websocket.recv()
-        print(f"<<< {greeting}")
+            greeting = await websocket.recv()
+            print(f"<<< {greeting}")
 
 if __name__ == "__main__":
     asyncio.run(hello())
